a03e1e4b31dfbc62e3a321f781f596ad0444333a
sebastiankennedy <sebastiankennedy@foxmail.com>
2020-01-28 21:12:20 +0800

    feat: UDP 客户端、UDP 服务端

--- 02/udp/client.php
+++ 02/udp/client.php
@@ -1,15 +1,28 @@
 <?php
 
-require "../../config.php";
+error_reporting(E_ERROR);
+
+require '../../config.php';
 
 // 创建同步客户端
 $client = new Swoole\Client(SWOOLE_SOCK_UDP);
 
 // 发送数据
-$client->sendto(IP_ADDRESS, PORT, 'I am Client.');
+$client->sendto(IP_ADDRESS, PORT, 'Hello Server, I am a Udp Sync Client.') || exit('客户端发送信息失败');
 
-// 接受消息，倘若服务端不发送消息，会一直阻塞，不会关闭连接
-echo $client->recv();
+// 接受消息
+$bool = true;
+while ($bool) {
+    usleep(1000);
+    $data = $client->recv(65535, Swoole\Client::MSG_WAITALL);
+
+    if ($data) {
+        echo $data . PHP_EOL;
+        $bool = false;
+    } else {
+        echo swoole_strerror(swoole_last_error()) . PHP_EOL;
+    }
+}
 
 // 关闭连接
 $client->close();
--- 02/udp/server.php
+++ 02/udp/server.php
@@ -2,12 +2,31 @@
 
 require '../../config.php';
 
-// 创建 UDP Server 实例，监听 0.0.0.0:9190 端口
-$server = new Swoole\Server(HOST, PORT, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
+// 创建 UDP Server 实例
+$server = new Swoole\Server(HOST, PORT, SWOOLE_BASE, SWOOLE_UDP);
 
-$server->on('packet', function ($server, $data, $clientInfo) {
-    $server->sendto($clientInfo['address'], $clientInfo['port'], '服务端回声：' . $data . PHP_EOL);
-});
+$server->on(
+    'start',
+    function ($server) {
+        echo '服务端正在监听 ' . HOST . ':' . PORT . ' 端口' . PHP_EOL;
+    }
+);
+
+// 监听数据接收
+$server->on(
+    'packet',
+    function ($server, $data, $clientInfo) {
+        echo '客户端 ' . $clientInfo['address'] . ':' . $clientInfo['port'] . ' 发送信息如下：' . PHP_EOL;
+        echo $data . PHP_EOL;
+
+        // 向客户端发送数据
+        $server->sendto(
+            $clientInfo['address'],
+            $clientInfo['port'],
+            'Hello, ' . $clientInfo['address'] . ':' . $clientInfo['port'] . ', I am Your Father.' . PHP_EOL
+        );
+    }
+);
 
 // 启动服务端
 $server->start();
