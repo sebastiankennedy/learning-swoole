4c7c06b0c3a43c7752e0583c7659df2311e0da7a
sebastiankennedy <sebastiankennedy@foxmail.com>
2020-01-29 21:09:39 +0800

    refactor: TCP 粘包解决方案 - 固定包头包体

--- 03/buffer/client.php
+++ 03/buffer/client.php
@@ -2,35 +2,42 @@
 
 require "../../config.php";
 
-// 创建同步客户端
-$client = new Swoole\Client(SWOOLE_TCP, SWOOLE_ASYNC);
+go(
+    function () {
+        // 创建同步客户端
+        $client = new Swoole\Client(SWOOLE_TCP);
 
-// 配置客户端
-$client->set([
-    'open_length_check' => true,
-    // 设置包长度的类型
-    'package_length_type' => 'N',
-    // 包头从第几个字节开始计算
-    'package_length_offset' => 0,
-    // 包体从第几个字节开始计算
-    'package_body_offset' => 4,
-    // 包头包体合计大小
-    'package_max_length' => 1024 * 1024 * 2,
-]);
+        // 配置客户端
+        $client->set(
+            [
+                'open_length_check' => true,
+                // 设置包长度的类型
+                'package_length_type' => 'N',
+                // 包头从第几个字节开始计算
+                'package_length_offset' => 0,
+                // 包体从第几个字节开始计算
+                'package_body_offset' => 4,
+                // 包头包体合计大小
+                'package_max_length' => 1024 * 1024 * 2,
+            ]
+        );
 
-// 发送连接请求
-$client->connect(IP_ADDRESS, PORT)
-|| exit("Connect failed. Error: {$client->errCode}. \n");
+        // 发送连接请求
+        $client->connect(IP_ADDRESS, PORT)
+        || exit("Connect failed. Error: {$client->errCode}. \n");
 
-// 一次性发送大量的数据
-$body = json_encode(str_repeat('a', 1024 * 1024 * 1));
-$data = pack('N', strlen($body)) . $body;
-$client->send($data);
+        // 一次性发送大量的数据
+        $body = json_encode(str_repeat('a', 1024 * 1024 * 1));
+        $data = pack('N', strlen($body)) . $body;
+        $client->send($data);
 
-// 发送多条数据
-for ($i = 0; $i < 10; $i++) {
-    $client->send(pack('N', strlen('123456')) . '123456');
-}
+        // 发送多条数据
+        for ($i = 0; $i < 10; $i++) {
+            $data = mt_rand(1, 9999999);
+            $client->send(pack('N', strlen($data)) . $data);
+        }
 
-// 关闭连接
-$client->close();
+        // 关闭连接
+        $client->close();
+    }
+);
